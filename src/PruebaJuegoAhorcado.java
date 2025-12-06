f8ee97519867b1b01ebdc6a56f913a81b7186b1b
Jesus Govanny Valdez C <jesus.vc@tecjerez.edu.mx>
2025-12-06 16:21:49 -0600

    Creacion de la clase oportunidades

--- src/PruebaJuegoAhorcado.java
+++ src/PruebaJuegoAhorcado.java
@@ -15,6 +15,27 @@ interface ahorcadoInterface {
     public void eliminarArchivo();
 }
 
+class Oportunidades {
+    private byte intentos = 0;
+    
+    public byte getIntentos() {
+        return intentos;
+    }
+    
+    public void setIntentos(byte intentos) {
+        this.intentos = intentos;
+    }
+    
+    public void descontarIntentos() {
+        intentos = (byte)(intentos - 1);
+    }
+    
+    public void letrañ() {
+        intentos = (byte)(intentos + 1);
+    }
+}
+
+
 
 
 
