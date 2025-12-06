9b5a13d910643ce3116ec98b54e6077c37fdf5e7
Jesus Govanny Valdez C <jesus.vc@tecjerez.edu.mx>
2025-12-06 15:39:42 -0600

    Prueba de verificacion de cambios

--- src/PruebaJuegoAhorcado.java
+++ src/PruebaJuegoAhorcado.java
@@ -3,8 +3,8 @@ public class PruebaJuegoAhorcado {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Mensaje de prueba final");
-
+		System.out.println("Mensaje de prueba final verificacion de agregacion de cambios");
+		System.out.println("Mensaje de confirmacion");
 	}
 
 }
